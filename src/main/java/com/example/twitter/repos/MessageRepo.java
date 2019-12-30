7104a6e79f2aa556e4bd49414568171909c2b85c
Yurchuk Oleg <xauz151@gmail.com>
2019-12-30 19:58:18 +0200

    Added JPA with postgres

---
+++ src/main/java/com/example/twitter/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.example.twitter.repos;
+
+import com.example.twitter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
